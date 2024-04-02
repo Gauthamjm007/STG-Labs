f81586c40cc6a2723d4426803e8d951e61f7397f
gouthamjm <goutham@blocktheory.com>
2024-04-02 12:43:25 +0530

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+
+import { ThemeProvider } from "@emotion/react";
+import theme from "./theme";
+
+import { CssBaseline } from "@mui/material";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
